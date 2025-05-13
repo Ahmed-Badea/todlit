d8a79ae28b72e97c481ca0e8446eda066253a497
Ahmed-Badea <ahmedbadea158@gmail.com>
2025-05-13 04:30:47 +0300

    fix date format issue

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,9 +1,14 @@
 export const formatDate = (date: Date | string) => {
-  if (date instanceof Date) {
+  if (typeof date === "string") {
+    date = new Date(date);
+  }
+
+  if (date instanceof Date && !isNaN(date.getTime())) {
     const offsetDate = new Date(
       date.getTime() - date.getTimezoneOffset() * 60000
     );
     return offsetDate.toISOString().slice(0, 10);
   }
-  return date;
+
+  return date; // Returns unchanged if the string can't be converted to a valid Date
 };
